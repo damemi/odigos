2afa74d64c3480eda75bc42d3a403ac4ccb7d999
Mike Dame <mike@odigos.io>
2025-06-16 14:16:08 -0400

    Switch to byte return value

--- hooks/go/go_hooks.go
+++ hooks/go/go_hooks.go
@@ -8,8 +8,8 @@ import (
 // If no span is found in the context, it returns an empty SpanContext.
 //
 //go:noinline
-func GetTraceContext(ctx context.Context) string {
-	traceId := "none"
+func GetTraceContext(ctx context.Context) []byte {
+	traceId := []byte("00-00000000000000000000000000000000-0000000000000000-00")
 	return traceId
 }
 
