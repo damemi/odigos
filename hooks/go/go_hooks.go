698665b079721a3f6dce90ce2282727c0b79202d
Mike Dame <mike@odigos.io>
2025-06-16 14:38:28 -0400

    Add TraceId and SpanID funcs

--- hooks/go/go_hooks.go
+++ hooks/go/go_hooks.go
@@ -4,15 +4,29 @@ import (
 	"context"
 )
 
-// GetTraceContext extracts the trace.SpanContext from the provided context.
-// If no span is found in the context, it returns an empty SpanContext.
+// GetTraceContext extracts the full W3C trace context from the provided Go context.
+// If no span is found in the context, it returns a zero trace context.
 //
 //go:noinline
-func GetTraceContext(ctx context.Context) []byte {
-	traceId := []byte("00-00000000000000000000000000000000-0000000000000000-00")
+func GetW3CTraceContext(ctx context.Context) []byte {
+	traceContext := []byte("00-00000000000000000000000000000000-0000000000000000-00")
+	return traceContext
+}
+
+// GetTraceID returns the current trace ID from the provided Go context.
+// If no span is found in the context, it returns a zero ID.
+//
+//go:noinline
+func GetTraceID(ctx context.Context) []byte {
+	traceId := []byte("00000000000000000000000000000000")
 	return traceId
 }
 
-func secondHelper(i int) bool {
-	return i%2 == 0
+// GetSpanID returns the current span ID from the provided Go context.
+// If no span is found in the context, it returns a zero ID.
+//
+//go:noinline
+func GetSpanID(ctx context.Context) []byte {
+	spanId := []byte("0000000000000000")
+	return spanId
 }
